ef7ab67b2fb9f389e9b84b67de329b24d8fa1b38
genin00 <prtma631@gmail.com>
2026-06-25 01:03:57 +0800

    debug supabase env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -6,6 +6,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+alert("SUPABASE URL = " + supabaseUrl);
+alert("SUPABASE KEY = " + (supabaseKey ? "ADA" : "TIDAK ADA"));
+
 if (!supabaseUrl || !supabaseKey) {
   console.error('❌ Supabase URL atau Key tidak ditemukan di .env');
 }
